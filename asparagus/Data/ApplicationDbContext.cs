c5455cdc97f064645f0dde36f70730babc0d2143
xxcuzz <xxcuzzx@gmail.com>
2020-02-27 03:44:01 +0300

    Добавьте файлы проекта.

---
+++ asparagus/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using asparagus.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace asparagus.Data {
+    public class ApplicationDbContext : IdentityDbContext {
+        public DbSet<ApplicationUser> Users { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+            Database.EnsureCreated();
+
+        }
+    }
+}
---
+++ asparagus/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace asparagus.Models {
+    public class ApplicationUser {
+        //[Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public static int m_Counter = 0;
+    }
+}
